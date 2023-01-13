b4b0ffff911a53453e892a7d05f4aeefa732a5ef
Kang Gao <kang.g1988@gmail.com>
2023-01-13 14:55:58 +1300

    Add leaflet map package

--- src/containers/BodyContainer/components/MapBox/MapBox.js
+++ src/containers/BodyContainer/components/MapBox/MapBox.js
@@ -1,6 +1,20 @@
 import React from "react";
 import styles from "./mapBox.module.css";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
 export default function MapBox() {
-  return <div className={styles.map}></div>;
+  const baseLayerUrl = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const baseAttribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      // scrollWheelZoom={false}
+      className={styles.map}
+    >
+      <TileLayer url={baseLayerUrl} attribution={baseAttribution} />
+    </MapContainer>
+  );
 }
