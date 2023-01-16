65d2f222e7730589e1b9254621a3fe3ed62e3698
Kang Gao <kang.g1988@gmail.com>
2023-01-17 00:26:12 +1300

    Make api call works and shows info on markers when clicks

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -4,8 +4,10 @@ const baseUrl = "http://api.weatherapi.com/v1";
 const currentUrl = "current.json";
 // const focastUrl = "focast.json";
 
-export const getWeatherApi = (city) => {
-  request
-    .get(`${baseUrl}/${currentUrl}?key=${APIkey}&p=${city}`)
-    .then((res) => res.body);
+export const getWeatherByGeoPositionApi = async ({ lat, lng }) => {
+  return request
+    .get(`${baseUrl}/${currentUrl}?key=${APIkey}&q=${lat},${lng}`)
+    .then((res) => {
+      return res.body;
+    });
 };
--- src/containers/BodyContainer/components/MapBox/controls/MarkerTool.js
+++ src/containers/BodyContainer/components/MapBox/controls/MarkerTool.js
@@ -1,6 +1,9 @@
 import React, { useState, useRef, useMemo, useCallback } from "react";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
+import { Icon, Grid, Popup as SemanticPopup } from "semantic-ui-react";
 import { v4 } from "uuid";
+import { getWeatherByGeoPositionApi } from "../../../../../api/weatherApi";
+import L from "leaflet";
 import _ from "lodash";
 
 export default function MarkerTool({ markers, setMarkers }) {
@@ -14,12 +17,17 @@ export default function MarkerTool({ markers, setMarkers }) {
   };
 
   useMapEvents({
-    click({ latlng }) {
+    click: async ({ latlng }) => {
+      const newMarker = _.cloneDeep(latlng);
       if (latlng) {
-        const newMark = _.cloneDeep(latlng);
-        newMark.id = v4();
-        newMark.draggable = false;
-        handleSaveResult(newMark);
+        const weatherResult = await getWeatherByGeoPositionApi(latlng);
+        if (weatherResult) {
+          newMarker.weather = weatherResult.current;
+          newMarker.location = weatherResult.location;
+          newMarker.id = v4();
+          newMarker.draggable = false;
+          handleSaveResult(newMarker);
+        }
       }
     },
   });
@@ -63,19 +71,51 @@ export default function MarkerTool({ markers, setMarkers }) {
       key={marker.id}
       draggable={marker.draggable}
       eventHandlers={eventHandlers}
+      icon={
+        marker.weather.condition.icon
+          ? L.icon({
+              iconUrl: marker.weather.condition.icon,
+              iconSize: [64, 64],
+              iconAnchor: [32, 32],
+            })
+          : L.Icon.Default()
+      }
       position={{
         lat: marker.lat,
         lng: marker.lng,
       }}
       ref={markerRef}
     >
-      <Popup minWidth={90}>
-        <span onClick={() => toggleDraggable(marker)}>
-          {marker.id}
-          {marker.draggable
-            ? "Marker is draggable"
-            : "Click here to make marker draggable"}
-        </span>
+      <Popup minWidth={180}>
+        <Grid columns={2}>
+          <Grid.Row stretched>
+            <SemanticPopup
+              content={
+                marker.draggable
+                  ? "Click to lock current marker"
+                  : "Click to make marker draggable"
+              }
+              trigger={
+                <Icon
+                  name={marker.draggable ? "lock red" : "move green"}
+                  onClick={() => toggleDraggable(marker)}
+                />
+              }
+            />
+          </Grid.Row>
+          <Grid.Row columns="equal">
+            <Grid.Column>{marker.location.name}</Grid.Column>
+            <Grid.Column>{marker.location.localtime}</Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns="equal">
+            <Grid.Column>{marker.location.region}</Grid.Column>
+            <Grid.Column>{marker.weather.condition.text}</Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns="equal">
+            <Grid.Column>{marker.location.country}</Grid.Column>
+            <Grid.Column>{marker.weather.temp_c}℃</Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Popup>
     </Marker>
   ));
