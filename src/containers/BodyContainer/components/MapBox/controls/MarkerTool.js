47ad2cdacadabfd920c2f0e65363bda9d1440051
Kang Gao <kang.g1988@gmail.com>
2023-01-17 15:08:45 +1300

    remove draggable and only be able to save and delete markers

--- src/containers/BodyContainer/components/MapBox/controls/MarkerTool.js
+++ src/containers/BodyContainer/components/MapBox/controls/MarkerTool.js
@@ -1,19 +1,30 @@
 import React, { useState, useRef, useMemo, useCallback } from "react";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
-import { Icon, Grid, Popup as SemanticPopup } from "semantic-ui-react";
+import { Button, Icon, Grid, Popup as SemanticPopup } from "semantic-ui-react";
 import { v4 } from "uuid";
 import { getWeatherByGeoPositionApi } from "../../../../../api/weatherApi";
 import L from "leaflet";
 import _ from "lodash";
 
 export default function MarkerTool({ markers, setMarkers }) {
-  const [draggingMarker, setDraggingMarker] = useState(null);
+  // const [draggingMarker, setDraggingMarker] = useState(null);
+  const [activeMarker, setActiveMarker] = useState(null);
   const markerRef = useRef(null);
 
-  const handleSaveResult = (marker) => {
+  const handleSaveMarker = (e) => {
+    e.preventDefault();
     const savedResult = _.cloneDeep(markers);
-    savedResult.push(marker);
+    savedResult.push(activeMarker);
     setMarkers(savedResult);
+    setActiveMarker(null);
+  };
+
+  const handleDeleteMarker = (e, markerForDelete) => {
+    e.preventDefault();
+    const filteredMarkers = markers.filter(
+      (marker) => marker.id !== markerForDelete.id
+    );
+    setMarkers(filteredMarkers);
   };
 
   useMapEvents({
@@ -26,97 +37,158 @@ export default function MarkerTool({ markers, setMarkers }) {
           newMarker.location = weatherResult.location;
           newMarker.id = v4();
           newMarker.draggable = false;
-          handleSaveResult(newMarker);
+          setActiveMarker(newMarker);
         }
       }
     },
   });
 
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker = markerRef.current;
-        if (marker != null && draggingMarker != null) {
-          const markerLatLng = marker.getLatLng();
-          const dragedMarkers = markers.map((marker) => {
-            if (marker.id === draggingMarker.id) {
-              marker.lat = markerLatLng.lat;
-              marker.lng = markerLatLng.lng;
-            }
-            return marker;
-          });
-          setMarkers(dragedMarkers);
-        }
-      },
-    }),
-    [draggingMarker, markers, setMarkers]
-  );
+  // const eventHandlers = useMemo(
+  //   () => ({
+  //     dragend() {
+  //       const marker = markerRef.current;
+  //       if (marker != null && draggingMarker != null) {
+  //         const markerLatLng = marker.getLatLng();
+  //         const dragedMarkers = markers.map((marker) => {
+  //           if (marker.id === draggingMarker.id) {
+  //             marker.lat = markerLatLng.lat;
+  //             marker.lng = markerLatLng.lng;
+  //           }
+  //           return marker;
+  //         });
+  //         setMarkers(dragedMarkers);
+  //       }
+  //     },
+  //   }),
+  //   [draggingMarker, markers, setMarkers]
+  // );
 
-  const toggleDraggable = useCallback(
-    (markerForDrag) => {
-      const toggledMarkers = markers.map((marker) => {
-        if (marker.id === markerForDrag.id) {
-          marker.draggable = !marker.draggable;
-          setDraggingMarker(marker);
-        }
-        return marker;
-      });
-      setMarkers(toggledMarkers);
-    },
-    [markers, setMarkers]
-  );
+  // const toggleDraggable = useCallback(
+  //   (markerForDrag) => {
+  //     const toggledMarkers = markers.map((marker) => {
+  //       if (marker.id === markerForDrag.id) {
+  //         marker.draggable = !marker.draggable;
+  //         setDraggingMarker(marker);
+  //       }
+  //       return marker;
+  //     });
+  //     setMarkers(toggledMarkers);
+  //   },
+  //   [markers, setMarkers]
+  // );
 
-  return markers.map((marker, index) => (
-    <Marker
-      key={marker.id}
-      draggable={marker.draggable}
-      eventHandlers={eventHandlers}
-      icon={
-        marker.weather.condition.icon
-          ? L.icon({
-              iconUrl: marker.weather.condition.icon,
-              iconSize: [64, 64],
-              iconAnchor: [32, 32],
-            })
-          : L.Icon.Default()
-      }
-      position={{
-        lat: marker.lat,
-        lng: marker.lng,
-      }}
-      ref={markerRef}
-    >
-      <Popup minWidth={180}>
-        <Grid columns={2}>
-          <Grid.Row stretched>
-            <SemanticPopup
-              content={
-                marker.draggable
-                  ? "Click to lock current marker"
-                  : "Click to make marker draggable"
-              }
-              trigger={
-                <Icon
-                  name={marker.draggable ? "lock red" : "move green"}
-                  onClick={() => toggleDraggable(marker)}
-                />
-              }
-            />
-          </Grid.Row>
-          <Grid.Row columns="equal">
-            <Grid.Column>{marker.location.name}</Grid.Column>
-            <Grid.Column>{marker.location.localtime}</Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns="equal">
-            <Grid.Column>{marker.location.region}</Grid.Column>
-            <Grid.Column>{marker.weather.condition.text}</Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns="equal">
-            <Grid.Column>{marker.location.country}</Grid.Column>
-            <Grid.Column>{marker.weather.temp_c}℃</Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Popup>
-    </Marker>
-  ));
+  return (
+    <>
+      {activeMarker && (
+        <Marker
+          icon={
+            activeMarker.weather.condition.icon
+              ? L.icon({
+                  iconUrl: activeMarker.weather.condition.icon,
+                  iconSize: [64, 64],
+                  iconAnchor: [32, 32],
+                })
+              : L.Icon.Default()
+          }
+          position={{
+            lat: activeMarker.lat,
+            lng: activeMarker.lng,
+          }}
+        >
+          <Popup minWidth={180}>
+            <Grid columns={2}>
+              <Grid.Row columns="equal">
+                <Grid.Column>{activeMarker.location.name}</Grid.Column>
+                <Grid.Column>{activeMarker.location.localtime}</Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal">
+                <Grid.Column>{activeMarker.location.region}</Grid.Column>
+                <Grid.Column>{activeMarker.weather.condition.text}</Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal">
+                <Grid.Column>{activeMarker.location.country}</Grid.Column>
+                <Grid.Column>{activeMarker.weather.temp_c}℃</Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal">
+                <Grid.Column>
+                  <span style={{ color: "blue", cursor: "pointer" }}>
+                    more details
+                  </span>
+                </Grid.Column>
+                <Grid.Column>
+                  <SemanticPopup
+                    content={"Save marker."}
+                    trigger={
+                      <Icon
+                        style={{ cursor: "pointer" }}
+                        onClick={(e) => handleSaveMarker(e)}
+                        color="green"
+                        name="save"
+                      />
+                    }
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Popup>
+        </Marker>
+      )}
+      {markers.map((marker, index) => (
+        <Marker
+          key={marker.id}
+          icon={
+            marker.weather.condition.icon
+              ? L.icon({
+                  iconUrl: marker.weather.condition.icon,
+                  iconSize: [64, 64],
+                  iconAnchor: [32, 32],
+                })
+              : L.Icon.Default()
+          }
+          position={{
+            lat: marker.lat,
+            lng: marker.lng,
+          }}
+          ref={markerRef}
+        >
+          <Popup minWidth={180}>
+            <Grid columns={2}>
+              <Grid.Row columns="equal">
+                <Grid.Column>{marker.location.name}</Grid.Column>
+                <Grid.Column>{marker.location.localtime}</Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal">
+                <Grid.Column>{marker.location.region}</Grid.Column>
+                <Grid.Column>{marker.weather.condition.text}</Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal">
+                <Grid.Column>{marker.location.country}</Grid.Column>
+                <Grid.Column>{marker.weather.temp_c}℃</Grid.Column>
+              </Grid.Row>
+              <Grid.Row stretched>
+                <Grid.Column>
+                  <span style={{ color: "blue", cursor: "pointer" }}>
+                    more details
+                  </span>
+                </Grid.Column>
+                <Grid.Column>
+                  <SemanticPopup
+                    content={"Delete marker."}
+                    trigger={
+                      <Icon
+                        style={{ cursor: "pointer" }}
+                        onClick={(e) => handleDeleteMarker(e, marker)}
+                        color="red"
+                        name="close"
+                      />
+                    }
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Popup>
+        </Marker>
+      ))}
+    </>
+  );
 }
