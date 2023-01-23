b6d1f18e125a7a753cbd9cac3b2018c20fa36582
Kang Gao <kang.g1988@gmail.com>
2023-01-23 23:05:22 +1300

    Move functions inside useEffect out

--- client/containers/BodyContainer/BodyContainer.js
+++ client/containers/BodyContainer/BodyContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import HeaderBar from "../HeaderBar";
 import { LoginContext } from "../../store/loginContext";
 import styles from "./bodyContainer.module.css";
@@ -15,58 +15,65 @@ export default function BodyContainer() {
   const loginState = useContext(LoginContext.State);
   const loginDispatch = useContext(LoginContext.Dispatch);
 
+  const renewMarkers = useCallback(async (markersForRenew) => {
+    const promises = await markersForRenew.map(async (marker) => {
+      const position = {
+        lat: marker.lat,
+        lng: marker.lng,
+      };
+      const result = await getWeatherByGeoPositionApi(position);
+      if (result) {
+        marker.weather = result.current;
+        marker.location = result.location;
+      }
+      return marker;
+    });
+    return await Promise.all(promises);
+  }, []);
+
+  const validateUserToken = useCallback(
+    async (token) => {
+      const result = await validateUserApi(token);
+      if (result.error) {
+        loginDispatch({
+          type: LoginContext.types.LOGOUT,
+        });
+        toast.error(result.message);
+      } else {
+        const payload = {
+          authToken: result.user.token,
+          userProfile: {
+            firstname: result.user.firstname,
+            lastname: result.user.lastname,
+            email: result.user.email,
+          },
+          markers: result.user.markers,
+        };
+        loginDispatch({
+          type: LoginContext.types.LOGIN,
+          payload,
+        });
+        const updatedMarkers = await renewMarkers(result.user.markers);
+        setMarkers(updatedMarkers);
+      }
+    },
+    [loginDispatch, renewMarkers]
+  );
+
+  const updateSavedMarkers = useCallback(
+    async (localMarkers) => {
+      if (localMarkers.lenth === 0) {
+        setMarkers([]);
+        return;
+      }
+      const updatedMarkers = await renewMarkers(localMarkers);
+      setMarkers(updatedMarkers);
+    },
+    [renewMarkers]
+  );
+
   useEffect(() => {
     try {
-      const renewMarkers = async (markersForRenew) => {
-        const promises = await markersForRenew.map(async (marker) => {
-          const position = {
-            lat: marker.lat,
-            lng: marker.lng,
-          };
-          const result = await getWeatherByGeoPositionApi(position);
-          if (result) {
-            marker.weather = result.current;
-            marker.location = result.location;
-          }
-          return marker;
-        });
-        return await Promise.all(promises);
-      };
-
-      const validateUserToken = async (token) => {
-        const result = await validateUserApi(token);
-        if (result.error) {
-          loginDispatch({
-            type: LoginContext.types.LOGOUT,
-          });
-          toast.error(result.message);
-        } else {
-          const payload = {
-            authToken: result.user.token,
-            userProfile: {
-              firstname: result.user.firstname,
-              lastname: result.user.lastname,
-              email: result.user.email,
-            },
-            markers: result.user.markers,
-          };
-          loginDispatch({
-            type: LoginContext.types.LOGIN,
-            payload,
-          });
-          const updatedMarkers = await renewMarkers(result.user.markers);
-          setMarkers(updatedMarkers);
-        }
-      };
-      const updateSavedMarkers = async (localMarkers) => {
-        if (localMarkers.lenth === 0) {
-          setMarkers([]);
-          return;
-        }
-        const updatedMarkers = await renewMarkers(localMarkers);
-        setMarkers(updatedMarkers);
-      };
-
       if (!loginState.loggedIn) {
         const authToken = window.localStorage.getItem("authToken");
         // handle markers when user not loged in
@@ -99,7 +106,13 @@ export default function BodyContainer() {
         });
       });
     }
-  }, [loginDispatch, loginState.loggedIn, loginState.markers, userLocation]);
+  }, [
+    loginState.loggedIn,
+    loginState.markers,
+    updateSavedMarkers,
+    userLocation,
+    validateUserToken,
+  ]);
 
   useEffect(() => {
     if (markers) {
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ server.use("/fonts", express.static(resolve(__dirname, "./fonts")));
 // server.use(cors('*'))
 
 const userRoute = require("./routes/userRoute");
-const markerRoute = require("./routes/markerRoute")
+const markerRoute = require("./routes/markerRoute");
 
 server.use("/api/v1/users", userRoute);
 server.use("/api/v1/markers", markerRoute);
