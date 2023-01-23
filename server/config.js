60d983e40eabfa5882209855a1cbc9323cc9f584
Kang Gao <kang.g1988@gmail.com>
2023-01-23 23:53:09 +1300

    changed database uri for deploy

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-const mongoURI = "mongodb://0.0.0.0:27017/weatherapp";
-// const mongoURI = process.env.mongoURI
+// const mongoURI = "mongodb://0.0.0.0:27017/weatherapp";
+const mongoURI = process.env.mongoURI;
 
 module.exports = { mongoURI };
